d8c8e78cba811d26db40d7805386b358c707c2cb
shorab.rustam <shorab.rustam@mpokket.com>
2024-04-28 22:23:36 +0530

    creaet alian Class and getter and setter

---
+++ src/main/java/com/example/FirstProject/Alian.java
@@ -0,0 +1,31 @@
+package com.example.FirstProject;
+
+public class Alian {
+    private int aid;
+    private String aname;
+    private String tech;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+}
