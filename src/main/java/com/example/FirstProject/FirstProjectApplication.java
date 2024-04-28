53047345181aafc5ef2e05cba1f7b2d5a1ba3492
shorab.rustam <shorab.rustam@mpokket.com>
2024-04-28 20:04:02 +0530

    Welcome to Spring Boot Project !!!

--- src/main/java/com/example/FirstProject/FirstProjectApplication.java
+++ src/main/java/com/example/FirstProject/FirstProjectApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Boot Project !!!");
 		SpringApplication.run(FirstProjectApplication.class, args);
+		System.out.println("Welcome to Spring Boot Project !!!");
 	}
 
 }
