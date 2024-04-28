22635f91c11274b1805bdef2aed218ebf95db3d5
shorab.rustam <shorab.rustam@mpokket.com>
2024-04-28 22:42:34 +0530

    using bean call the alian method

--- src/main/java/com/example/FirstProject/FirstProjectApplication.java
+++ src/main/java/com/example/FirstProject/FirstProjectApplication.java
@@ -1,14 +1,19 @@
 package com.example.FirstProject;
 
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstProjectApplication.class, args);
-		System.out.println("Welcome to Spring Boot Project !!!");
+//		SpringApplication.run(FirstProjectApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstProjectApplication.class, args);
+		Alian alienObj = context.getBean(Alian.class);
+		alienObj.show();
+//		System.out.println("Welcome to Spring Boot Project !!!");
 	}
 
 }
